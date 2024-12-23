cf486e74bfd27371626fecdc5926e011aaa6a3e2
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-12-23 14:52:06 -0500

    fix tests in outline.rs

--- crates/forge-tool/src/outline.rs
+++ crates/forge-tool/src/outline.rs
@@ -1,6 +1,6 @@
+use std::collections::{HashMap, HashSet};
 use std::path::Path;
 
-use indexmap::{IndexMap, IndexSet};
 use schemars::JsonSchema;
 use serde::Deserialize;
 use tokio::fs;
@@ -22,8 +22,8 @@ fn load_language_parser(language_name: &str) -> Result<Language, String> {
     }
 }
 
-fn load_queries() -> IndexMap<&'static str, &'static str> {
-    let mut queries = IndexMap::new();
+fn load_queries() -> HashMap<&'static str, &'static str> {
+    let mut queries = HashMap::new();
     queries.insert("rust", RUST);
     queries.insert("javascript", JAVASCRIPT);
     queries.insert("python", PYTHON);
@@ -35,7 +35,7 @@ fn parse_file(_file: &Path, content: &str, parser: &mut Parser, query: &Query) -
     let mut cursor = QueryCursor::new();
     let mut formatted_output = String::new();
     let mut last_line = -1;
-    let mut seen_lines = IndexSet::new();
+    let mut seen_lines = HashSet::new();
 
     let mut captures: Vec<_> = cursor
         .matches(query, tree.root_node(), content.as_bytes())
@@ -97,10 +97,10 @@ impl ToolTrait for Outline {
 
     async fn call(&self, input: Self::Input) -> Result<Self::Output, String> {
         let extensions_to_languages =
-            IndexMap::from([("rs", "rust"), ("js", "javascript"), ("py", "python")]);
+            HashMap::from([("rs", "rust"), ("js", "javascript"), ("py", "python")]);
 
         let queries = load_queries();
-        let mut parsers: IndexMap<&str, (Parser, Query)> = IndexMap::new();
+        let mut parsers: HashMap<&str, (Parser, Query)> = HashMap::new();
         let mut result = String::new();
 
         let entries = WalkDir::new(&input.path)
@@ -288,6 +288,11 @@ class Person:
             .await
             .unwrap();
 
+        let seperator = "\n|----\n";
+        let mut result = result.split(seperator).collect::<Vec<_>>();
+        result.sort();
+        result = result.iter().map(|x| x.trim()).collect();
+        let result = result.join(seperator);
         assert_snapshot!("outline_multiple_files", result);
     }
 
