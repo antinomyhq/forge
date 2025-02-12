1c563ca70375f1b444ab9fbb1160ec147780b65f
Sandipsinh Rathod <sandip@ssdd.dev>
2025-02-12 15:05:54 -0500

    drop unwanted builds

--- crates/forge_ci/tests/ci.rs
+++ crates/forge_ci/tests/ci.rs
@@ -26,41 +26,41 @@ fn generate() {
                 "binary_path": "target/x86_64-unknown-linux-gnu/debug/forge",
                 "cross": "false"
             },
-            {
-                "os": "ubuntu-latest",
-                "target": "aarch64-unknown-linux-gnu",
-                "binary_name": "forge-aarch64-unknown-linux-gnu",
-                "binary_path": "target/aarch64-unknown-linux-gnu/debug/forge",
-                "cross": "true"
-            },
-            {
-                "os": "macos-latest",
-                "target": "x86_64-apple-darwin",
-                "binary_name": "forge-x86_64-apple-darwin",
-                "binary_path": "target/x86_64-apple-darwin/debug/forge",
-                "cross": "false"
-            },
-            {
-                "os": "macos-latest",
-                "target": "aarch64-apple-darwin",
-                "binary_name": "forge-aarch64-apple-darwin",
-                "binary_path": "target/aarch64-apple-darwin/debug/forge",
-                "cross": "false"
-            },
-            {
-                "os": "windows-latest",
-                "target": "x86_64-pc-windows-msvc",
-                "binary_name": "forge-x86_64-pc-windows-msvc.exe",
-                "binary_path": "target/x86_64-pc-windows-msvc/debug/forge.exe",
-                "cross": "false"
-            },
-            {
-                "os": "windows-latest",
-                "target": "aarch64-pc-windows-msvc",
-                "binary_name": "forge-aarch64-pc-windows-msvc.exe",
-                "binary_path": "target/aarch64-pc-windows-msvc/debug/forge.exe",
-                "cross": "false"
-            }
+            // {
+            //     "os": "ubuntu-latest",
+            //     "target": "aarch64-unknown-linux-gnu",
+            //     "binary_name": "forge-aarch64-unknown-linux-gnu",
+            //     "binary_path": "target/aarch64-unknown-linux-gnu/debug/forge",
+            //     "cross": "true"
+            // },
+            // {
+            //     "os": "macos-latest",
+            //     "target": "x86_64-apple-darwin",
+            //     "binary_name": "forge-x86_64-apple-darwin",
+            //     "binary_path": "target/x86_64-apple-darwin/debug/forge",
+            //     "cross": "false"
+            // },
+            // {
+            //     "os": "macos-latest",
+            //     "target": "aarch64-apple-darwin",
+            //     "binary_name": "forge-aarch64-apple-darwin",
+            //     "binary_path": "target/aarch64-apple-darwin/debug/forge",
+            //     "cross": "false"
+            // },
+            // {
+            //     "os": "windows-latest",
+            //     "target": "x86_64-pc-windows-msvc",
+            //     "binary_name": "forge-x86_64-pc-windows-msvc.exe",
+            //     "binary_path": "target/x86_64-pc-windows-msvc/debug/forge.exe",
+            //     "cross": "false"
+            // },
+            // {
+            //     "os": "windows-latest",
+            //     "target": "aarch64-pc-windows-msvc",
+            //     "binary_name": "forge-aarch64-pc-windows-msvc.exe",
+            //     "binary_path": "target/aarch64-pc-windows-msvc/debug/forge.exe",
+            //     "cross": "false"
+            // }
         ]
     });
 
