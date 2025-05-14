e24561e844bd22245dd3132e240a28149b88b91e
Tushar Mathur <tusharmath@gmail.com>
2025-05-14 11:01:41 +0530

    feat: enhance CI workflow with new build jobs and dependencies

--- crates/forge_ci/src/workflow.rs
+++ crates/forge_ci/src/workflow.rs
@@ -5,7 +5,7 @@ use crate::jobs;
 
 /// Generate the main CI workflow
 pub fn generate_ci_workflow() {
-    let mut workflow = StandardWorkflow::default()
+    let workflow = StandardWorkflow::default()
         .auto_fix(true)
         .to_ci_workflow()
         .concurrency(Concurrency {
@@ -17,16 +17,21 @@ pub fn generate_ci_workflow() {
 
     // Get the jobs
     let build_job = workflow.jobs.clone().unwrap().get("build").unwrap().clone();
-    let draft_release_job = jobs::create_draft_release_job();
+    let draft_release_job = jobs::create_draft_release_job(&build_job);
 
     // Add jobs to the workflow
-    workflow = workflow.add_job("draft_release", draft_release_job.clone());
-    workflow = workflow.add_job(
-        "build-release",
-        jobs::create_build_release_main_job(&build_job, &draft_release_job),
-    );
-
-    workflow.generate().unwrap();
+    workflow
+        .add_job("draft_release", draft_release_job.clone())
+        .add_job(
+            "build_release",
+            jobs::create_build_release_main_job(&draft_release_job),
+        )
+        .add_job(
+            "build_release_pr",
+            jobs::create_build_release_pr_job(&draft_release_job),
+        )
+        .generate()
+        .unwrap();
 }
 
 /// Generate homebrew release workflow
