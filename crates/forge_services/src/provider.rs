1e1843986cdd08021b13f8620baec2cf74807311
laststylebender14 <ran.mahadik14@gmail.com>
2025-07-18 16:22:51 +0530

    - add TODO

--- crates/forge_services/src/provider.rs
+++ crates/forge_services/src/provider.rs
@@ -83,6 +83,8 @@ impl ProviderService for ForgeProviderService {
 
         // Models not in cache, fetch from client
         let client = self.client(provider).await?;
+
+        // TODO: we should retry if error is transient
         let models = client.models().await?;
 
         // Cache the models
