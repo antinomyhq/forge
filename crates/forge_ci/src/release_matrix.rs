15bb75963c4aeda8dc8ca2aa958325488000197b
Sandipsinh Rathod <zotbysandip@gmail.com>
2025-07-28 12:57:24 -0400

    chore: update release configuration for x86_64 musl builds

--- crates/forge_ci/src/release_matrix.rs
+++ crates/forge_ci/src/release_matrix.rs
@@ -18,20 +18,20 @@ impl Default for ReleaseMatrix {
     /// Returns a vector of all build matrix entries
     fn default() -> Self {
         ReleaseMatrix(vec![
-            // MatrixEntry {
-            //     os: "ubuntu-latest",
-            //     target: "x86_64-unknown-linux-musl",
-            //     binary_name: "forge-x86_64-unknown-linux-musl",
-            //     binary_path: "target/x86_64-unknown-linux-musl/release/forge",
-            //     cross: "true",
-            // },
             MatrixEntry {
                 os: "ubuntu-latest",
-                target: "aarch64-unknown-linux-musl",
-                binary_name: "forge-aarch64-unknown-linux-musl",
-                binary_path: "target/aarch64-unknown-linux-musl/build/forge",
+                target: "x86_64-unknown-linux-musl",
+                binary_name: "forge-x86_64-unknown-linux-musl",
+                binary_path: "target/x86_64-unknown-linux-musl/debug/forge",
                 cross: "true",
             },
+            // MatrixEntry {
+            //     os: "ubuntu-latest",
+            //     target: "aarch64-unknown-linux-musl",
+            //     binary_name: "forge-aarch64-unknown-linux-musl",
+            //     binary_path: "target/aarch64-unknown-linux-musl/release/forge",
+            //     cross: "true",
+            // },
             // MatrixEntry {
             //     os: "ubuntu-latest",
             //     target: "x86_64-unknown-linux-gnu",
