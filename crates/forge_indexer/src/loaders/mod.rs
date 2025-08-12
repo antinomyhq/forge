b1b3a7c7ce0c29839fc33208a09ba593442ba91d
Tushar Mathur <tusharmath@gmail.com>
2025-08-12 16:43:27 +0530

    feat: add tree-sitter and tree-sitter-rust dependencies; refactor loaders for chunk processing

--- crates/forge_indexer/src/loaders/mod.rs
+++ crates/forge_indexer/src/loaders/mod.rs
@@ -1,9 +1,9 @@
-use std::{path::PathBuf};
+use std::path::PathBuf;
 
-use futures::StreamExt;
-// use futures::future::TryFutureExt;
-use futures::stream::TryStreamExt;
+use futures::{FutureExt, StreamExt, TryStreamExt};
 use serde::{Deserialize, Serialize};
+use text_splitter::{ChunkCapacity, ChunkConfig};
+use tree_sitter::Language;
 
 use crate::loaders::file_loader::FileLoader;
 
@@ -25,13 +25,13 @@ pub trait Loader {
 pub struct Chunk {
     pub content: String,
     pub path: PathBuf,
-    pub pos: Position,
     pub hash: String,
+    pub position: Position,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Position {
-    pub start: usize,
+    pub chat_offset: usize,
     pub end: usize,
 }
 
@@ -45,40 +45,44 @@ pub trait Embedder {
     async fn batch_embed(&self, nodes: &[Node]) -> anyhow::Result<Vec<Vec<u8>>>;
 }
 
-async fn example() {
+fn hash(content: &str) -> String {
+    todo!()
+}
+
+async fn example() -> anyhow::Result<()> {
     struct FileRead {
         path: PathBuf,
         content: String,
     }
 
-    // 1. Loader
-    // let output = FileLoader::new("path/to/directory", vec![".rs".into()])
-    //     .flat_map(|paths| tokio_stream::iter(paths.into_iter()))
-    //     .chunks(128)
-    //     .then(|paths| {
-    //         futures::future::join_all(
-    //             paths
-    //                 .into_iter()
-    //                 .map(|path| {
-    //                     FileRead {
-    //                         path: path.clone(),
-    //                         content: tokio::fs::read_to_string(path),
-    //                     }
-    //                 }),
-    //         )
-    //     }).then(|chunks| {
-    //         todo!()
-    //     });
+    let config = ChunkConfig::new(ChunkCapacity::new(1024));
+    let splitter = text_splitter::CodeSplitter::new(tree_sitter_rust::LANGUAGE, config)?;
 
     // 1. Loader
     let output = FileLoader::new("path/to/directory", vec![".rs".into()])
         .flat_map(|paths| tokio_stream::iter(paths.into_iter()))
-        .and_then(|path| {
+        .map(|path| {
             tokio::fs::read_to_string(path.clone())
                 .map(|content| content.map(|c| FileRead { content: c, path }))
         })
-        .buffer_unordered(128)
-        .map(|file| {});
+        .buffer_unordered(1024)
+        .flat_map(|file| {
+            let file = file.unwrap();
+            let content = file.content;
+            let content = splitter
+                .chunk_char_indices(&content)
+                .map(|item| Chunk {
+                    path: file.path.to_owned(),
+                    hash: hash(item.chunk),
+                    position: Position {
+                        chat_offset: item.char_offset,
+                        end: item.char_offset + item.chunk.chars().count(),
+                    },
+                    content: item.chunk.to_owned(),
+                })
+                .collect::<Vec<_>>();
+            tokio_stream::iter(content)
+        });
     // .then(|file| todo!());
 
     // 2. create code semantic chunks
