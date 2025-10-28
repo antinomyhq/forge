0159ebe2f2244cb6bd3b87ce14484d5663e5c4ee
Amit Singh <amitksingh1490@gmail.com>
2025-10-28 12:31:55 +0530

    refactor: remove provider_credentials table and related migrations after migrating to JSON file storage

--- crates/forge_infra/src/database/migrations/2025-10-21-000000_create_provider_credentials/down.sql
+++
@@ -1,6 +0,0 @@
--- Drop indexes
-DROP INDEX IF EXISTS idx_provider_credentials_auth_type;
-DROP INDEX IF EXISTS idx_provider_credentials_provider_id;
-
--- Drop table
-DROP TABLE IF EXISTS provider_credentials;
--- crates/forge_infra/src/database/migrations/2025-10-28-000000_drop_provider_credentials/up.sql
+++
@@ -1,2 +0,0 @@
--- Drop provider_credentials table as we've migrated to JSON file storage
-DROP TABLE IF EXISTS provider_credentials;
