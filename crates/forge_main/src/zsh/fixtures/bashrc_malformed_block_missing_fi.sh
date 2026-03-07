5abc2dacee6ac96543a348f6563b7e0b1035dd17
Sandipsinh Dilipsinh Rathod <62684960+ssddOnTop@users.noreply.github.com>
2026-03-07 03:05:05 -0500

    test(zsh): add bashrc test fixtures for installation scenarios

---
+++ crates/forge_main/src/zsh/fixtures/bashrc_malformed_block_missing_fi.sh
@@ -0,0 +1,9 @@
+# My bashrc
+export PATH=$PATH:/usr/local/bin
+
+# Added by zsh installer
+if [ -t 0 ] && [ -x "/usr/bin/zsh" ]; then
+  export SHELL="/usr/bin/zsh"
+
+# Content after incomplete block (will be lost)
+alias ll='ls -la'
