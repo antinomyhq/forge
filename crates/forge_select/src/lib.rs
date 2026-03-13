492878710927e7f13b0f6eaeab4e55358b3e02b8
Tushar <tusharmath@gmail.com>
2026-03-13 17:10:04 +0530

    refactor(forge_select): extract SelectBuilder to separate module

--- crates/forge_select/src/lib.rs
+++ crates/forge_select/src/lib.rs
@@ -1,7 +1,9 @@
 mod input;
 mod multi;
+mod select;
 mod widget;
 
 pub use input::InputBuilder;
 pub use multi::MultiSelectBuilder;
-pub use widget::{ForgeWidget, SelectBuilder};
+pub use select::SelectBuilder;
+pub use widget::ForgeWidget;
