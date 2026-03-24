23819a9e2d7a11a13d4a731af21ce583a28a68d6
Tushar <tusharmath@gmail.com>
2026-03-24 16:56:48 +0530

    refactor(forge_config): rename forge_config module to config

--- crates/forge_config/src/lib.rs
+++ crates/forge_config/src/lib.rs
@@ -1,11 +1,11 @@
 mod auto_dump;
-mod forge_config;
+mod config;
 mod http_config;
 mod model_config;
 mod retry_config;
 
 pub use auto_dump::*;
-pub use forge_config::*;
+pub use config::*;
 pub use http_config::*;
 pub use model_config::*;
 pub use retry_config::*;
