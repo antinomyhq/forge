061755d2a325a0dcff46d9b04e6c8ab521f1e0fb
Zakir Chalimov <z.chalimov@yandex.ru>
2026-04-08 21:15:43 +0600

    fix: remove unused release_homebrew and release_npm jobs

--- crates/forge_ci/src/jobs/mod.rs
+++ crates/forge_ci/src/jobs/mod.rs
@@ -8,8 +8,6 @@ mod release_build_job;
 mod release_docker;
 mod release_draft;
 mod release_draft_pr;
-mod release_homebrew;
-mod release_npm;
 
 pub use bounty_job::*;
 pub use draft_release_update_job::*;
@@ -19,5 +17,3 @@ pub use release_build_job::*;
 pub use release_docker::*;
 pub use release_draft::*;
 pub use release_draft_pr::*;
-pub use release_homebrew::*;
-pub use release_npm::*;
